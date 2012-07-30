f66dece3e56f3355b94d008af7e1a1b0db0e58b3
Parkour <paintedostrich@gmail.com>
2012-07-29 23:20:46 -0700

    fixed capitalization issue and debugged email

--- sendEmailToTakeTest2.php
+++ sendEmailToTakeTest2.php
@@ -4,12 +4,12 @@
 $hourSpan = 5;
 
 // Test values between now and hourSpan ago
-$timeAgoMin = time();
-$timAgoMax = $hourSpan * 60 * 60;
+// $timeAgoMin = time();
+// $timAgoMax = $hourSpan * 60 * 60;
 // end test values
 
-// $timeAgoMin = time() - (24 * 60 * 60);
-// $timAgoMax = time() - (($hourSpan + 24) * 60 * 60);
+$timeAgoMin = time() - (24 * 60 * 60);
+$timAgoMax = time() - (($hourSpan + 24) * 60 * 60);
 
 echo $earliestTime;
 
@@ -18,25 +18,20 @@ $select = "SELECT * FROM email WHERE time > ".$timAgoMax." AND time < ".$timeAgo
 $dbh = new PDO('mysql:host=localhost;dbname=psych', 'root', 'root'); 
 
 foreach ($dbh->query($select) as $entry) 
-{
-	echo $entry['email'];
-	
+{	
 	$to      = $entry['email'];
 	$message = "Thank you for your interest in participating in the second part of the study you completed yesterday.  Today, you will have the opportunity to obtain an estimate of a type of long-term face-name memory IQ.  This study is being conducted by cognitive scientists at Washington University in St Louis.  Your participation is completely voluntary and you are free to discontinue at any time.  Please click on the link below if you wish to participate.\n\n ";
 	$message = $message."http://experiments.wustl.edu/day2.html?ln=".$entry['testNumber']."&ud=".$entry['userid'];
 	$subject = 'Partcipate in Part 2 of your Face-Name Memory IQ Test';
 	$headers = 'From: memdart@wustl.edu' . "\r\n" .
-	    'Reply-To: webmaster@example.com' . "\r\n";
+	    'Reply-To: memdart@wustl.edu' . "\r\n";
 
-	print_r($message);
+	// print_r("to: ".$to."</br>");
+	// print_r("mess ".$message."</br>");
+	// print_r("sub: ".$subject."</br>");
 	mail($to, $subject, $message, $headers);
 }
 
-
-// Missing from and password
 //http://php.net/manual/en/function.mail.php
-// When sending mail, the mail must contain a From header. This can be set with the additional_headers parameter, or a default can be set in php.ini.
-
-// Failing to do this will result in an error message similar to Warning: mail(): "sendmail_from" not set in php.ini or custom "From:" header missing. The From header sets also Return-Path under Windows.
 
 ?>
